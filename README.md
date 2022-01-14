d5f578ace0f556ae29f54769467c0ff39a0e913e
Songkit Phachareon <severx002@gmail.com>
2022-01-14 13:30:10 +0700

    re-write logic

--- README.md
+++ README.md
@@ -1,21 +1,65 @@
-Helper functions for create media query rules
+Detect screen size and write media query
+<br/>
 
 #### Example
 
 ```
-const mq = require('mq')
+import { mq } from 'rexiduz-mq'
 
-const rule = mq.getter("_EXPRESSIONS._MOBILE_LANDSCAPE")
-const mediaRule = mq.createRule(rule)
-
-
-console.log(mediaRule)
+console.log(mq('md')) >> '@media (min-width: 1024px)'
+console.log(mq('md', false)) >> '(min-width: 1024px)'
 ```
 
-**output**
+#### ReactJs
 
 ```
-@media
-      screen and (pointer: coarse) and (orientation: landscape) and (max-width: 832px),
-      screen and (orientation: landscape) and (max-width: 832px)
+import { useBreakpoint } from 'rexiduz-mq'
+
+...
+
+const { screens } = useBreakpoint()
+
+return (
+      <>
+            { screens.mobile && <h1>Hi Mobile</h1> }
+            { screens.tablet && <h1>Hi Tablet</h1> }
+            { screens.desktop && <h1>Hi Desktop</h1> }
+            { screens.xs && <h1>Hi Screen size 'xs'</h1> }
+            { screens.sm && <h1>Hi Screen size 'sm'</h1> }
+            { screens.md && <h1>Hi Screen size 'md'</h1> }
+            { screens.lg && <h1>Hi Screen size 'lg'</h1> }
+            { screens.xl && <h1>Hi Screen size 'xl'</h1> }
+            { screens.xxl && <h1>Hi Screen size 'xxl'</h1> }
+            { screens.xxxl && <h1>Hi Screen extra sizes</h1> }
+      </>
+)
+```
+
+#### Override `breakpoints`
+
+```
+import { breakpoints, mq } from 'rexiduz-mq'
+
+
+console.log(breakpoints.lg) >> 1280
+
+// override
+breakpoints.lg = 1300
+
+console.log(breakpoints.lg) >> 1300
+console.log(mq('lg')) >> '@media (min-width: 1300px)'
+console.log(mq('lg', false)) >> '(min-width: 1300px)'
+
+```
+
+#### Integrate with `styled-components`
+
+```
+const Container = styled.div`
+      background: red;
+
+      ${mq('lg')} {
+            background: yellow;
+      }
+`
 ```
--- index.js
+++ index.js
@@ -1,2 +1,72 @@
-module.exports = require('./src')
-module.exports.CONSTANTS = require('./src/constants')
+const useMediaQuery = require('react-responsive').useMediaQuery
+const last = (list = []) => list[list.length - 1]
+
+const breakpoints = {
+  xs: 512,
+  sm: 768,
+  md: 1024,
+  lg: 1280,
+  xl: 1440,
+  xxl: 1680,
+  xxxl: 1920
+}
+
+const getWidth = (breakpoint = 'md') => {
+  return breakpoints[breakpoint]
+}
+
+const mq = (size, symbol = true) => {
+  const words = []
+  const breakpoint = getWidth(size)
+  const screenSize = breakpoint ? `${breakpoint}px` : size
+
+  if (symbol) {
+    words.push('@media')
+  }
+
+  words.push(`(min-width: ${screenSize})`)
+
+  return words.join(' ')
+}
+
+const vh = (size) => {
+  return `calc(var(--vh, 1vh) * ${size})`
+}
+
+const useBreakpoint = () => {
+  const xs = useMediaQuery({ query: mq('xs', false) })
+  const sm = useMediaQuery({ query: mq('sm', false) })
+  const md = useMediaQuery({ query: mq('md', false) })
+  const lg = useMediaQuery({ query: mq('lg', false) })
+  const xl = useMediaQuery({ query: mq('xl', false) })
+  const xxl = useMediaQuery({ query: mq('xxl', false) })
+  const xxxl = useMediaQuery({ query: mq('xxxl', false) })
+
+  const matchesStrings = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxl', 'xxxl']
+  const matches = [xs, sm, md, lg, xl, xxl, xxl, xxxl]
+  const matchedIndex = matches.lastIndexOf(true) + 1
+
+  const finalMatchedIndex =
+    matchedIndex === matchesStrings.length
+      ? matchesStrings.length - 1
+      : matchedIndex
+
+  const breakpoint = matchesStrings[finalMatchedIndex]
+  const screenSizes = matchesStrings.filter(
+    (_, index) => index <= finalMatchedIndex
+  )
+
+  const screens = screenSizes.reduce((a, c) => ({ ...a, [c]: true }), {})
+
+  screens.mobile = ['xs'].includes(last(screenSizes))
+  screens.tablet = ['sm', 'md'].includes(last(screenSizes))
+  screens.desktop = !!screens.lg
+
+  return { screens, current: { size: { [breakpoint]: true }, breakpoint } }
+}
+
+module.exports.getWidth = getWidth
+module.exports.mq = mq
+module.exports.vh = vh
+module.exports.useBreakpoint = useBreakpoint
+module.exports.breakpoints = breakpoints
